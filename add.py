d50da56ed9abc314d49eb1b61b1d4754b1d3694e
Liza Cullis <lizac@aleios.com>
2023-09-14 16:45:04 +0200

    feat: add numbers

---
+++ add.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+# Example usage:
+num1 = 5
+num2 = 3
+sum_result = add_numbers(num1, num2)
+print("The sum is:", sum_result)
+
+add_numbers(3,8)
